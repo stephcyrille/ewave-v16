73586a3634446610a001ac79842aa55e2abbdc9c
Stephcyrille <stephcyril.sc@gmail.com>
2023-08-21 02:19:54 +0200

    Add a vendor field and a constrains for checking if the vendor is filled

--- nextewave_purchase/models/crm_lead.py
+++ nextewave_purchase/models/crm_lead.py
@@ -11,17 +11,22 @@ class NextewaveLeadCrmPurchase(models.Model):
     _description = 'NEXTeWave CRM Lead'
 
     vendor_order_count = fields.Integer(string="Number of Quotations", default=0)
+    vendor_id = fields.Many2one('res.partner', string='Vendor', change_default=True, tracking=True,
+                                 domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
+                                 help="You can find a vendor by its Name, TIN, Email or Internal Reference.")
 
     def action_goto_purchase_order_form(self):
         for rec in self:
             if not len(rec.crm_product_ids) > 0:
                 raise ValidationError("Products details error: You must first add system product before to continue")
+            if not rec.vendor_id:
+                raise ValidationError("Vendor is missing: You must add a vendor for processing")
             else:
                 action = self.env["ir.actions.actions"]._for_xml_id("purchase.purchase_rfq")
                 action['context'] = {
                     'search_default_draft': 1,
-                    'search_default_partner_id': rec.partner_id.id,
-                    'default_partner_id': rec.partner_id.id,
+                    'search_default_partner_id': rec.vendor_id.id,
+                    'default_partner_id': rec.vendor_id.id,
                     'default_partner_ref': rec.name,
                     'default_opportunity_id': rec.id,
                     'crm_products': [{'id': x.product_id.id, 'name': x.product_id.name, 'qty': x.product_qty, 'price': x.price_unit} for x in rec.crm_product_ids]
