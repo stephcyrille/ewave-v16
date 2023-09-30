73cf8eab6350ca22587110957a108132f9f88628
Stephcyrille <stephcyril.sc@gmail.com>
2023-09-30 15:08:08 +0200

    change states sequences in crm lead and fix visibility of the customer so button

--- nextewave_purchase/models/crm_sale_order.py
+++ nextewave_purchase/models/crm_sale_order.py
@@ -42,7 +42,7 @@ class NextewaveCrmSaleOrder(models.Model):
         if res.opportunity_id:
             # Check if we already have a vendor order
             crm_stage_obj = self.env['crm.stage']
-            stage = crm_stage_obj.sudo().search([('sequence', '=', 6)])
+            stage = crm_stage_obj.sudo().search([('sequence', '=', 5)])
             res.opportunity_id.write({
                 'quotation_count': 1,
                 'state': 'customer_so_created',
@@ -51,8 +51,6 @@ class NextewaveCrmSaleOrder(models.Model):
         return res
 
     def action_confirm(self):
-        # self.get_current_balance()
-        print("Ptrrrrrrrrrrrrrrrrrrrrrrrrr\n\n\n\n\n\n")
         if self._get_forbidden_state_confirm() & set(self.mapped('state')):
             raise UserError(_(
                 'It is not allowed to confirm an order in the following states: %s'
@@ -72,35 +70,35 @@ class NextewaveCrmSaleOrder(models.Model):
             self.action_done()
         return True
 
-    def action_confirm(self):
-        self.ensure_one()
-        self.get_current_balance()
-        print("Ptrrrrrrrrrrrrrrrrrrrrrrrrr\n\n\n\n\n\n")
-        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
-            raise UserError(_(
-                'It is not allowed to confirm an order in the following states: %s'
-            ) % (', '.join(self._get_forbidden_state_confirm())))
-
-        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-            order.message_subscribe([order.partner_id.id])
-        self.write(self._prepare_confirmation_values())
-
-        # Context key 'default_name' is sometimes propagated up to here.
-        # We don't need it and it creates issues in the creation of linked records.
-        context = self._context.copy()
-        context.pop('default_name', None)
-
-        self.with_context(context)._action_confirm()
-        if self.env.user.has_group('sale.group_auto_done_setting'):
-            self.action_done()
-
-        crm_stage_obj = self.env['crm.stage']
-        stage = crm_stage_obj.sudo().search([('sequence', '=', 7)])
-        self.opportunity_id.write({
-            'state': 'order_paid',
-            'stage_id': stage.id
-        })
-        return True
+    # def action_confirm(self):
+    #     self.ensure_one()
+    #     self.get_current_balance()
+    #     print("Ptrrrrrrrrrrrrrrrrrrrrrrrrr\n\n\n\n\n\n")
+    #     if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+    #         raise UserError(_(
+    #             'It is not allowed to confirm an order in the following states: %s'
+    #         ) % (', '.join(self._get_forbidden_state_confirm())))
+    #
+    #     for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+    #         order.message_subscribe([order.partner_id.id])
+    #     self.write(self._prepare_confirmation_values())
+    #
+    #     # Context key 'default_name' is sometimes propagated up to here.
+    #     # We don't need it and it creates issues in the creation of linked records.
+    #     context = self._context.copy()
+    #     context.pop('default_name', None)
+    #
+    #     self.with_context(context)._action_confirm()
+    #     if self.env.user.has_group('sale.group_auto_done_setting'):
+    #         self.action_done()
+    #
+    #     crm_stage_obj = self.env['crm.stage']
+    #     stage = crm_stage_obj.sudo().search([('sequence', '=', 7)])
+    #     self.opportunity_id.write({
+    #         'state': 'order_paid',
+    #         'stage_id': stage.id
+    #     })
+    #     return True
 
     def get_current_balance(self):
         self.ensure_one()
