066992af523a56a24b88ac25fb2f6d5528a14bb2
Stephcyrille <stephcyril.sc@gmail.com>
2023-10-16 19:02:57 +0200

    compute expected revenue from delta between vendor price and sale price and adding current revenue field in the buying campaign object

--- nextewave_base/models/campaign.py
+++ nextewave_base/models/campaign.py
@@ -18,7 +18,10 @@ class BuyingCampaign(models.Model):
     end_date = fields.Date("End date", tracking=True, required=True)
     product_count = fields.Integer(string='Number of products', compute='_count_products',
                                    readonly=True)
-    expected_revenue = fields.Float("Expected revenue", default=0, tracking=True, required=True)
+    expected_revenue = fields.Float("Expected revenue", default=0, tracking=True,
+                                    compute='_compute_expected_revenue', readonly=True)
+    current_revenue = fields.Float("Current revenue", default=0, tracking=True,
+                                   compute='_compute_current_revenue', readonly=True)
     order_id = fields.Many2one('sale.order', string='Sale order', tracking=True)
     products_ids = fields.One2many('campaign.product.line', 'buying_campaign_id', string='Products', tracking=True)
     state = fields.Selection([
@@ -46,6 +49,20 @@ class BuyingCampaign(models.Model):
 
     def _count_products(self):
         self.product_count = len(self.products_ids)
+
+    @api.depends('products_ids')
+    def _compute_expected_revenue(self):
+        for rec in self:
+            rec.expected_revenue = 0
+            if rec.products_ids:
+                for product in rec.products_ids:
+                    value = (product.price_unit - product.vendor_unit_price) * product.product_qty
+                    rec.expected_revenue += value
+
+    @api.depends('products_ids')
+    def _compute_current_revenue(self):
+        for rec in self:
+            rec.current_revenue = 0
 
     @api.depends('company_id')
     def _compute_user_company_ids(self):
--- nextewave_base/models/campaign_product.py
+++ nextewave_base/models/campaign_product.py
@@ -11,9 +11,10 @@ class CampaignProductLine(models.Model):
 
     product_id = fields.Many2one('product.product', string='Product', required=False,
                                  tracking=True, domain="[('detailed_type', '=', 'product')]")
-    description = fields.Text('Description')
+    description = fields.Text('Description', compute='_compute_description')
     product_qty = fields.Float('Quantity', required=True, tracking=True)
     price_unit = fields.Float('Price', compute='_compute_unit_price')
+    vendor_unit_price = fields.Float('Vendor Price', compute='_compute_vendor_unit_price')
     total_price = fields.Float('Total price', readonly=True, tracking=True,
                                compute='_compute_total_price')
     buying_campaign_id = fields.Many2one('buying.campaign', ondelete='cascade', invisible=True)
@@ -40,3 +41,24 @@ class CampaignProductLine(models.Model):
                 rec.price_unit = rec.product_id.list_price
             else:
                 rec.price_unit = 0
+
+    @api.depends('product_id')
+    def _compute_vendor_unit_price(self):
+        for rec in self:
+            """
+            Trigger the recompute the total price.
+            """
+            if rec.product_id:
+                rec.vendor_unit_price = rec.product_id.standard_price
+            else:
+                rec.vendor_unit_price = 0
+
+    def _compute_description(self):
+        for rec in self:
+            """
+            Compute description name from internal reference and product name.
+            """
+            if rec.product_id:
+                rec.description = f"{rec.product_id.default_code} - {rec.product_id.name}"
+            else:
+                rec.description = ''
