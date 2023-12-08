d505adcdb8d8ee04bee90f8d740051a72fc18957
Stephcyrille <stephcyril.sc@gmail.com>
2023-12-08 08:52:20 +0100

    implement js in  sourcing component

--- next_website/static/src/js/sourcing-for-you.js
+++ next_website/static/src/js/sourcing-for-you.js
@@ -3,24 +3,49 @@
 const productList = document.querySelector("#products-list");
 const productForm = productList.querySelector(".product-form");
 const addProductButton = document.querySelector("#add-product");
-let size = productList.childElementCount;
+//let size = productList.childElementCount;
 
 addProductButton.addEventListener("click", addProduct);
+
+/**
+  * Increase the counter of products when we trigger on add button action
+*/
+
+ // Product increment
+var i = 1;
+var size = 1;
+var tab_counter = 1;
+
 
 /**
  * removeProduct: the function remove a form for a new product
  */
 
 function removeProduct(index) {
-  let element = document.getElementById(index);
-  element.remove();
+  var product_number = index.split('-')[1];
+  
+  if(product_number < tab_counter){
+    console.log("You must select first the last product");
+  } else {
+    let element = document.getElementById(index);
+    element.remove();
+    tab_counter--;
+    size--
+  }
+
+  console.log("Count items >>>>>>", tab_counter)
 }
 
 /**
  * addProduct: the function add a form for a new product
  */
+
+
 function addProduct() {
   size++;
+  tab_counter ++
+
+  document.getElementsByName("product_counter")[0].value = tab_counter
 
   let element = productForm.cloneNode(true);
   let index = size;
@@ -44,41 +69,83 @@ function addProduct() {
                                     </div>`;
 
   name.innerHTML = `<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                        for="product-${index}-name">
+                        for="product_name_${index}">
                         Product <i class="text-red-600">*</i>
                     </label>
                     <input
                         class="appearance-none block w-full bg-white text-gray-700 border border-gray-300 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-green"
-                        id="product-${index}-name" name="product-${index}-name" type="text" placeholder="Product name" required>`;
+                        id="product_name_${index}" name="product_name_${index}" type="text" placeholder="Product name" required>`;
 
   quantity.innerHTML = `<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                            for="product-${index}-quantity">
+                            for="quantity_${index}">
                             Quantity <i class="text-red-600">*</i>
                         </label>
                         <input
                             class="appearance-none block w-full bg-white text-gray-700 border border-gray-300 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-green"
-                            id="product-${index}-quantity" name="product-${index}-quantity" value="1" type="number" min="1" placeholder="" required>`;
+                            id="quantity_${index}" name="quantity_${index}" value="1" type="number" min="1" placeholder="" required>`;
 
   description.innerHTML = `<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                for="product-${index}-description">
+                                for="product_description_${index}">
                                 Description <i class="text-red-600">*</i>
                             </label>
-                            <textarea id="product-${index}-description" name="product-${index}-description" rows="3"
+                            <textarea id="product_description_${index}" name="product_description_${index}" rows="3"
                                 placeholder="Additional information of product here" required
                                 class="block w-full rounded border border-gray-300 text-gray-700 placeholder:text-gray-400 leading-normal py-3 px-4 mb-4 focus:bg-white focus:border-blue-green"></textarea>`;
 
   images.innerHTML = `<div class="w-full md:w-1/2 px-3 mb-6">
-                            <input id="product-${index}-file-1" name="product-${index}-file-1" type="file" class="r-only">
+                        <input type="file" class="r-only" name="product_picture_${index}_1"
+                               accept="image/jpeg,image/gif,image/png" data-show-upload="true"
+                               data-show-caption="true" lass="file" data-show-preview="true"
+                               id="product_picture_${index}_1" onchange="onAddPic(this)">
                         </div>
                         <div class="w-full md:w-1/2 px-3 mb-6">
-                            <input id="product-${index}-file-2" name="product-${index}-file-2" type="file" class="r-only">
+                            <input type="file" class="r-only" name="product_picture_${index}_2"
+                               accept="image/jpeg,image/gif,image/png" data-show-upload="true"
+                               data-show-caption="true" lass="file" data-show-preview="true"
+                               id="product_picture_${index}_2" onchange="onAddPic(this)">
                         </div>
                         <div class="w-full md:w-1/2 px-3 mb-6">
-                            <input id="product-${index}-file-3" name="product-${index}-file-3" type="file" class="r-only">
+                            <input type="file" class="r-only" name="product_picture_${index}_3"
+                               accept="image/jpeg,image/gif,image/png" data-show-upload="true"
+                               data-show-caption="true" lass="file" data-show-preview="true"
+                               id="product_picture_${index}_3" onchange="onAddPic(this)">
                         </div>
                         <div class="w-full md:w-1/2 px-3 mb-2 md:mb-0">
-                            <input id="product-${index}-file-4" name="product-${index}-file-4" type="file" class="r-only">
+                            <input type="file" class="r-only" name="product_picture_${index}_4"
+                               accept="image/jpeg,image/gif,image/png" data-show-upload="true"
+                               data-show-caption="true" lass="file" data-show-preview="true"
+                               id="product_picture_${index}_4" onchange="onAddPic(this)">
                         </div>`;
 
   productList.appendChild(element);
+}
+
+function onAddPic(input){
+    var id_pic = input.id
+
+    if (input.files && input.files[0]) {
+        if (!document.getElementById(`${id_pic}_prev`)){
+            var url = window.URL.createObjectURL(input.files[0])
+            var prevElt = `<div class='row mt-1' id='${id_pic}_prev'>
+                    <i class="fa fa-times text-danger text-center" onclick="delPrev(this)"/>
+                    <img src=${url} style="width: 200px; height: 100px"/>
+                </div>`
+            document.getElementById(id_pic)
+                .parentNode.insertAdjacentHTML("beforeend", prevElt);
+        } else {
+            document.getElementById(`${id_pic}_prev`).remove()
+            var url = window.URL.createObjectURL(input.files[0])
+            var prevElt = `<div class='row mt-1' id='${id_pic}_prev'>
+                    <i class="fa fa-times text-danger text-center" onclick="delPrev(this)"/>
+                    <img src=${url} style="width: 200px; height: 100px"/>
+                </div>`
+            document.getElementById(id_pic)
+                .parentNode.insertAdjacentHTML("beforeend", prevElt);
+        }
+    }
+}
+
+function delPrev(elt){
+    elt.parentNode.previousSibling.previousSibling.value= null;
+    elt.parentNode.remove();
 }
